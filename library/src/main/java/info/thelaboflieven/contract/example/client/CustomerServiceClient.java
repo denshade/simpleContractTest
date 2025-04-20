c6496a65c3a417c2b71358108db55eeac27e4e77
denshade <lieven.vaneeckhaute@gmail.com>
2025-04-20 14:56:11 +0200

    clean up.

--- library/src/main/java/info/thelaboflieven/contract/example/client/CustomerServiceClient.java
+++ library/src/main/java/info/thelaboflieven/contract/example/client/CustomerServiceClient.java
@@ -15,33 +15,17 @@ public class CustomerServiceClient {
 
     public String getCustomers() {
         try {
-            // Create a URL object from the string
             URL url = new URL(baseUrl+"/v1/customers");
-
-            // Open a connection to the URL
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            // Set the request method to GET
             connection.setRequestMethod("GET");
-
-            // Get the response code from the server
             int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            // Read the input stream if the response is OK (HTTP 200)
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                     String inputLine;
                     StringBuilder content = new StringBuilder();
-
-                    // Read the response line by line
                     while ((inputLine = in.readLine()) != null) {
                         content.append(inputLine);
                     }
-
-                    // Output the response content
-                    System.out.println("Response Content: ");
-                    System.out.println(content.toString());
                     return content.toString();
                 }
             } else {
